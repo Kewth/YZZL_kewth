55a639d3514ce65b73972f129e8c72dcb360efb1
Kewth <wthdsg@foxmail.com>
2018-08-02 17:36:45 +0800

    BUG

--- more/G.sh
+++ more/G.sh
@@ -2,5 +2,5 @@ reset
 g++ main.cpp umore.cpp \
 	people.cpp maincity.cpp cutcity.cpp funcity.cpp workcity.cpp firstcity.cpp \
 	war.cpp boss.cpp shuju.cpp bag.cpp jn.cpp \
-	-o ~/Desktop/Y"$(date "+%Y_%m_%d")" -lpthread -std=c++11
-~/Desktop/Y"$(date "+%Y_%m_%d")"
+	-o ./Y -lpthread -std=c++11
+./Y
