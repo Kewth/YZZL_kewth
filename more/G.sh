8b42fd3a052ae532d5266d1a91683e1fb21b0678
Kewth <wthdsg@foxmail.com>
2018-10-14 11:35:24 +0800

    更新G.sh

--- more/G.sh
+++ more/G.sh
@@ -1,6 +1,13 @@
-reset
-g++ ../main.cpp ../umore.cpp \
-	../people.cpp ../maincity.cpp ../cutcity.cpp ../funcity.cpp ../workcity.cpp ../firstcity.cpp \
-	../war.cpp ../shuju.cpp ../bag.cpp ../jn.cpp \
-	-o Y -lpthread -std=c++11
-./Y
+cd ../YZZL_kewth/ || exit
+# reset
+g++ main.cpp umore.cpp \
+	people.cpp maincity.cpp cutcity.cpp funcity.cpp workcity.cpp firstcity.cpp skycity.cpp \
+	war.cpp shuju.cpp bag.cpp jn.cpp \
+	-o ~/Desktop/Y"$(date "+%Y_%m_%d")" -lpthread -std=c++11
+if [ "$1" = -n ]
+then
+	zenity --info
+else
+	cd ~/Desktop || exit
+	./Y"$(date "+%Y_%m_%d")"
+fi
